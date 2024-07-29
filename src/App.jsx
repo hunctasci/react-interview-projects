f79fc480f095d97280705253d0d36a0b9f105df8
Hunc Tasci <htasci@apple.com>
2024-07-29 16:43:41 +0300

    random color and star rating components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <>
       {/* Accordion Component */}
-      <Accordion />
+      {/* <Accordion /> */}
+
+      {/* Random COlor Component */}
+      {/* <RandomColor /> */}
+
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10} />
     </>
   );
 }
