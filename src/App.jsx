844aa43b84a0a7668fb178fb9c10bdb740ccb0c8
Hunc Tasci <htasci@apple.com>
2024-07-30 15:50:13 +0300

    add load more button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Accordion from "./components/accordion";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -17,7 +18,10 @@ function App() {
       {/* <StarRating noOfStars={10} /> */}
 
       {/* Image Slider Component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} page={1} /> */}
+
+      {/* Load More Products Component */}
+      <LoadMoreData />
     </>
   );
 }
